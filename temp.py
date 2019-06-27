af9ca6c403f52d16acc92389eb89f6a612a36a9c
anmol1996 <anmol@gradeup.co>
2019-06-27 19:19:20 +0530

    2nd commit

--- temp.py
+++ temp.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
 """from nltk.stem import WordNetLemmatizer 
 lemmatizer = WordNetLemmatizer() 
-
-
 x = lemmatizer.lemmatize('study')
 y = lemmatizer.lemmatize('studying')
-
 print x,y"""
 
 
@@ -30,6 +25,7 @@ for w in words:
     
 import pandas as pd
 from pymongo import MongoClient
+import pprint 
 
 
 def _connect_mongo(host, port, username, password, db):
@@ -43,37 +39,63 @@ def _connect_mongo(host, port, username, password, db):
     return conn[db]
 
 
-def read_mongo(db, collection,  host='localhost', port=27017, username=None, password=None, no_id=True):
+def read_mongo(db, collection, query ,host='localhost', port=27017, username=None, password=None, no_id=True):
     """ Read from Mongo and Store into DataFrame """
 
     # Connect to MongoDB
     db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
-    query = [  { "$lookup": { "from": 'PriorityChanges', "localField": 'msid', "foreignField": 'msid', "as": 'priorities' } }, 
+    cursor = db[collection].aggregate(query)
+    return list(cursor)
+
+
+def find_mongo(db, collection, query ,host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+    cursor = db[collection].find(query)
+    return list(cursor)
+
+
+
+def write_mongo(db, collection, query,host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+    db[collection].insert(query)
+    return 
+
+def update_mongo(db, collection, query, keyword,host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+    db[collection].update({'keyword':keyword},query)
+    return 
+
+if __name__ == '__main__':
+    keywords = read_mongo('hackathon', 'Stories', [  { "$lookup": { "from": 'PriorityChanges', "localField": 'msid', "foreignField": 'msid', "as": 'priorities' } }, 
              { "$match": { "priorities": { "$ne": []},"keywords": { "$ne": [] },} },
-             {"$project": { "keywords": 1, "msid": 1,'priorities.rank': 1,'priorities.position': 1} }]
+             {"$project": { "keywords": 1, "msid": 1,'priorities.rank': 1,'priorities.position': 1} }])
+    
+    
+    msid = keywords[0][u'msid']
+    priorities = keywords[0][u'priorities']
+    
+    
+    for z in keywords:
+        for i in z[u'keywords']:
+            key_data = find_mongo('hackathon', 'KeywordsScore', {'keyword': i})
+            if len(key_data) == 0:    
+                write_mongo('hackathon', 'KeywordsScore', { 'keyword': i, 'rank': priorities[0][u'rank']  })
+            else:
+                rank = key_data[0][u'rank'];
+                update_mongo('hackathon', 'KeywordsScore', { 'keyword': i, 'rank': priorities[0][u'rank'] + rank}, i )
+            
+        
+    
     
         
     
-    #query2 = {'msid':69919513}
-    # Make a query to the specific DB and Collection
-    cursor = db[collection].aggregate(query)
-    
-    print list(cursor)    
-
-    # Expand the cursor and construct the DataFrame
-    df =  pd.DataFrame(list(cursor))
-    
-   # print df
-    # Delete the _id
-    if no_id and '_id' in df:
-        del df['_id']
-
-    return df
-
-if __name__ == '__main__':
-    df = read_mongo('hackathon', 'Stories')
-    df.to_csv('1.csv', index=False)
-    
-
-
     
